4f0d77fbb09ab7364f7bdf81e1cbd02aae12ae80
Jorge Bucaran <jbucaran@me.com>
2015-07-13 21:00:35 +0900

    declare output extension

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ module.exports = function () {
   return this.filter("babel", (src, opts) => {
     try { return require("babel").transform(src, opts).code }
     catch (e) { throw e }
-  })
+  }, { ext: ".js" })
 }
