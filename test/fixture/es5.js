ed7de653e845323309b3e7d957558645bf0791fb
Jorge Bucaran <jbucaran@me.com>
2015-08-04 00:15:27 +0900

    add tests :boom:

---
+++ test/fixture/es5.js
@@ -0,0 +1,5 @@
+"use strict";
+
+var _a$b = { a: 1, b: 2 };
+var a = _a$b.a;
+var b = _a$b.b;
--- test/index.js
+++ test/index.js
@@ -1,6 +1,14 @@
 const test = require("tape").test
+const bind = require("fly-util").bind
+const babel = require(bind("../"))
 
-test("fly-babel", function (t) {
-  t.ok(true, "ok")
-  t.end()
+test("fly-babel", (t) => {
+  t.plan(3)
+  babel.call({
+    filter: function (name, transform, options) {
+      t.equal(name, "babel", "add babel filter")
+      t.ok(/var a/.test(transform("let a = 0")), "babel transform")
+      t.equal(options.ext, ".js", "extension is .js")
+    }
+  })
 })
