06d12a4545f7c1e38f9e46157345e8983df18172
luke <luke.edwards05@gmail.com>
2016-01-07 11:56:52 -0800

    fix transform test. needed es2015 preset

--- test/index.js
+++ test/index.js
@@ -5,7 +5,7 @@ test("fly-babel", (t) => {
   t.plan(3)
   babel.call({
     filter: function (name, transform) {
-      const result = transform("let a = 0")
+      const result = transform("let a = 0", {presets: ["es2015"]})
       t.equal(name, "babel", "add babel filter")
       t.ok(/var a/.test(result.code), "babel transform")
       t.equal(result.ext, ".js", "extension is .js")
