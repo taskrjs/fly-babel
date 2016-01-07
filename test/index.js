bff218c608fb37690f316a05407b79e94dcd3cc7
luke <luke.edwards05@gmail.com>
2016-01-07 12:10:24 -0800

    add tests for sourcemaps

--- test/index.js
+++ test/index.js
@@ -2,13 +2,18 @@ const test = require("tape").test
 const babel = require("../")
 
 test("fly-babel", (t) => {
-  t.plan(3)
+  t.plan(5)
   babel.call({
     filter: function (name, transform) {
-      const result = transform("let a = 0", {presets: ["es2015"]})
+      const content = "let a = 0"
+      const result = transform(content, {presets: ["es2015"]})
+      const map = transform(content, {sourceMaps: true}).map
+
       t.equal(name, "babel", "add babel filter")
       t.ok(/var a/.test(result.code), "babel transform")
       t.equal(result.ext, ".js", "extension is .js")
+      t.equal(result.map, null, "no sourcemaps by default")
+      t.equal(map.sourcesContent[0], content, "turn on sourcemaps")
     }
   })
 })
