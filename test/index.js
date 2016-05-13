80afe757625d35bbcd752c0d80657164994e2e99
luke <luke.edwards05@gmail.com>
2016-05-13 10:42:59 -0700

    tabs: 2

--- test/index.js
+++ test/index.js
@@ -1,19 +1,19 @@
-var test = require("tape").test
-var babel = require("../")
+const test = require('tape').test
+const babel = require('../')
 
-test("fly-babel", function (t) {
-  t.plan(5)
-  babel.call({
-    filter: function (name, transform) {
-      const content = "let a = 0"
-      const result = transform(content, {file: {}, presets: ["es2015"]})
-      const map = transform(content, {file: {}, sourceMaps: true}).map
+test('fly-babel', function (t) {
+	t.plan(5)
+	babel.call({
+		filter: function (name, transform) {
+			const content = 'let a = 0'
+			const result = transform(content, {file: {}, presets: ['es2015']})
+			const map = transform(content, {file: {}, sourceMaps: true}).map
 
-      t.equal(name, "babel", "add babel filter")
-      t.ok(/var a/.test(result.code), "babel transform")
-      t.equal(result.ext, ".js", "extension is .js")
-      t.equal(result.map, null, "no sourcemaps by default")
-      t.equal(map.sourcesContent[0], content, "turn on sourcemaps")
-    }
-  })
+			t.equal(name, 'babel', 'add babel filter')
+			t.ok(/var a/.test(result.code), 'babel transform')
+			t.equal(result.ext, '.js', 'extension is .js')
+			t.equal(result.map, null, 'no sourcemaps by default')
+			t.equal(map.sourcesContent[0], content, 'turn on sourcemaps')
+		}
+	})
 })
