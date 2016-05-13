088d8e8f4f65846ad81af7e9094d00359acf73a4
Luke Edwards <luke.edwards05@gmail.com>
2016-05-13 09:53:14 -0700

    update for fly 1.3 (#7)
    * update for fly 1.3
    
    babel needs `filename` and can't handle extras
    
    * remove fly-util
    
    * fix tests
    
    * udpate travis for more node envs
    
    * fix test cmd
    
    * remove fat arrow for iojs
    
    * damn iojs

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const babel = require("babel-core").transform
 const assign = require("object-assign")
 
 module.exports = function () {
-  return this.filter("babel", (data, options) => {
+  return this.filter("babel", function (data, options) {
+    options.filename = options.file.base
+    delete options.file
     return assign({ ext: ".js"}, babel(data.toString(), options))
   })
 }
--- test/index.js
+++ test/index.js
@@ -1,13 +1,13 @@
-const test = require("tape").test
-const babel = require("../")
+var test = require("tape").test
+var babel = require("../")
 
-test("fly-babel", (t) => {
+test("fly-babel", function (t) {
   t.plan(5)
   babel.call({
     filter: function (name, transform) {
       const content = "let a = 0"
-      const result = transform(content, {presets: ["es2015"]})
-      const map = transform(content, {sourceMaps: true}).map
+      const result = transform(content, {file: {}, presets: ["es2015"]})
+      const map = transform(content, {file: {}, sourceMaps: true}).map
 
       t.equal(name, "babel", "add babel filter")
       t.ok(/var a/.test(result.code), "babel transform")
