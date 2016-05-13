e69c947e6239f188cd6138919d1eec7f914d5d4b
Luke Edwards <luke.edwards05@gmail.com>
2016-05-13 09:25:45 -0700

    update for fly 1.3
    babel needs `filename` and can't handle extras

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const assign = require("object-assign")
 
 module.exports = function () {
   return this.filter("babel", (data, options) => {
+    options.filename = options.file.base
+    delete options.file
     return assign({ ext: ".js"}, babel(data.toString(), options))
   })
 }
