741852f4e2e5b5683d6d884be45889d347afa69e
hzlmn <qualityfurniture@mail.ua>
2016-08-20 20:47:30 +0300

    add preset preloading; parses package names (#9)
    * added preload state
    
    * updated regex and cache

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const babel = require("babel-core").transform
 const assign = require("object-assign")
+const readPkg = require("read-pkg-up")
+const BABEL_REGEX = /(^babel-preset)-(.*)/ig
 
 module.exports = function () {
+  var cache
   return this.filter("babel", function (data, options) {
+    if (options.preload) {
+      var pkg = cache || (cache = readPkg.sync().pkg)
+      var deps = pkg.devDependencies || {}
+      var presets = Object.keys(deps).reduce(function (acc, value) {
+        var processed = BABEL_REGEX.exec(value)
+        if (!processed) return acc
+        acc.push(processed[2])
+        return acc
+      }, [])
+      options.presets = presets
+      delete options.preload
+    }
     options.filename = options.file.base
     delete options.file
     return assign({ ext: ".js"}, babel(data.toString(), options))
