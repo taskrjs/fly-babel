9c52ccfc7110908acba62d044b6e221a10d89f27
hzlmn <qualityfurniture@mail.ua>
2016-08-25 23:41:30 +0300

    added tests for plugin preloading; de-dupe deps parsing (#12)
    * added tests for plugins preloading, cleanup preset/plugin matching
    
    * added t.end(), convert to regular string

--- index.js
+++ index.js
@@ -2,23 +2,23 @@ const babel = require('babel-core').transform
 const assign = require('object-assign')
 const readPkg = require('read-pkg-up')
 
-function matchType(deps, type) {
-  var rgx = new RegExp('(^babel-' + type + ')-(.*)', 'ig')
-  return deps.reduce(function (acc, value) {
-    var matches = rgx.exec(value)
-    return matches ? (acc.concat(matches[2])) : acc
-  }, [])
-}
+var BABEL_REGEX = /(^babel-)(preset|plugin)-(.*)/i
 
 module.exports = function () {
   var cache
   return this.filter('babel', function (data, options) {
     if (options.preload) {
       delete options.preload
+
       var pkg = cache || (cache = readPkg.sync().pkg)
       var deps = Object.keys(pkg.devDependencies || {})
-      options.presets = matchType(deps, 'preset')
-      options.plugins = matchType(deps, 'plugin')
+
+      deps.forEach(function (value) {
+        var parsed = BABEL_REGEX.exec(value)
+        if (parsed) {
+          (options[parsed[2] + 's'] || (options[parsed[2] + 's'] = [])).push(parsed[3])
+        }
+      })
     }
     options.filename = options.file.base
     delete options.file
